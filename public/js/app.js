f00e0e5c4ffb2de83bee5a651ce9b27c46208875
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-02-05 16:54:09 -0800

    unsave handlebars fixed

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,7 @@ $('#scrape-btn').on('click', function () {
         });
 })
 
+
 $('#articles').on("click", '.save-btn', function () {
     // var thisId = $(this).closest('div.card').attr("data-id");
     var thisId = this.dataset.id.trim();
@@ -32,8 +33,7 @@ $('#saved-articles').on("click", '.del-save-btn', function () {
 
     $.ajax({
         method: "PUT",
-        url: "/api/unsaved/" + thisId,
-
+        url: "/api/unsaved/" + thisId + "/unsave",
     })
         .then(function (data) {
             console.log(data);
@@ -42,6 +42,7 @@ $('#saved-articles').on("click", '.del-save-btn', function () {
 
 });
 
+
 $('#saved-toggle').click(function () {
     $('#saved-articles').slideToggle();
 })
@@ -49,3 +50,20 @@ $('#saved-toggle').click(function () {
 $('#show-toggle').click(function () {
     $('#articles').slideToggle();
 })
+
+
+// $.getJSON("/api/articles", function (data) {
+//     // For each one
+//     console.log(data)
+//     for (var i = 0; i < data.length; i++) {
+//         $("#articles").append("<div class='col-lg-4 col-md-6 mb-4'><div class='card' data-id=" + data[i]._id + "><img class='card-img-top' src='" + data[i].image + "' alt=''><div class='card-body'><a href='" + data[i].link + "'><h4 class='card-title'>" + data[i].title + "</h4></a><p class='card-text'></p></div><div class='card-footer'><a data-id=" + data[i]._id + " class='save-btn btn btn-secondary'>Save Article</a></div></div></div>");
+//     }
+// });
+
+// $.getJSON("/api/saved", function (data) {
+//     // For each one
+//     console.log(data)
+//     for (var i = 0; i < data.length; i++) {
+//         $("#saved-articles").append("<div class='col-lg-4 col-md-6 mb-4'><div class='card' data-id=" + data[i]._id + "><img class='card-img-top' src='" + data[i].image + "' alt=''><div class='card-body'><a href='" + data[i].link + "'><h4 class='card-title'>" + data[i].title + "</h4></a><p class='card-text'></p></div><div class='card-footer'><a data-id=" + data[i]._id + " class='text-white del-save-btn btn btn-secondary'>Delete From Saved</a></div></div></div>");
+//     }
+// });
